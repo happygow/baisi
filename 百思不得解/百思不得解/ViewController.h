80f415744e9984c927565296a78c4d9191ceb3ed
sunnyday <sunnyday@sunnyday.local>
2016-05-15 13:10:12 +0800

    初始化项目

---
+++ 百思不得解/百思不得解/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  百思不得解
+//
+//  Created by sunnyday on 16/5/15.
+//  Copyright © 2016年 sunnyday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 百思不得解/百思不得解/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得解
+//
+//  Created by sunnyday on 16/5/15.
+//  Copyright © 2016年 sunnyday. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
